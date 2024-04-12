046755b39bc93f5d4fc479568a2f4dcda861c0b9
xBlank77 <127398500+xBlank77@users.noreply.github.com>
2024-04-12 21:09:42 +0800

    Added AccountController

---
+++ MVC-Midterm-Activity/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Midterm_Activity.Models;
+namespace MVC_Midterm_Activity.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Verify(Account acc)
+        {
+            return View();
+        }
+    }
+}
