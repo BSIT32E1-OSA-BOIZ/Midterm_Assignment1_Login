4665e6703c044245eb3f63e68b2eec2ec05d676e
xBlank77 <127398500+xBlank77@users.noreply.github.com>
2024-04-12 22:39:44 +0800

    Added UserRepository Class

---
+++ MVC-Midterm-Activity/Models/AccountsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Midterm_Activity.Models
+{
+    public class AccountsDbContext : DbContext
+    {
+        public AccountsDbContext(DbContextOptions options) : base(options)
+        { 
+        }
+
+        public DbSet<Account> Accounts { get; set;}
+    }
+}
---
+++ MVC-Midterm-Activity/Providers/Repositories/IUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using MVC_Midterm_Activity.Models;
+
+namespace MVC_Midterm_Activity.Providers.Repositories
+{
+    public interface IUserRepository
+    {
+        //...
+    }
+
+    public class UserRepository : IUserRepository
+    {
+        private readonly AccountsDbContext _db;
+
+        public UserRepository(AccountsDbContext db) 
+        { 
+            _db = db;
+        }
+
+        //...
+    }
+}
