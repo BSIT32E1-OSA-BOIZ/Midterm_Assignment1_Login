947b9e0629f7f59a11f0dfad4f2dd9c1449bb798
xBlank77 <127398500+xBlank77@users.noreply.github.com>
2024-04-12 21:00:27 +0800

    Add project files.

---
+++ MVC-Midterm-Activity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_Midterm_Activity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
